3fdebc58b1c468b0cd2892f95e3db12cdeddad18
Tim Jonker <tmjonker1@outlook.com>
2022-05-08 22:35:54 -0400

    Added change password functionality for admin user.

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function MainPage() {
   return (
     <div className="logo-div">
-      <img className="logo" src="./images/burger_bonanza.png"></img>
+      <img className="logo" src="./images/burger_bonanza.png" alt="logo"></img>
     </div>
   );
 }
